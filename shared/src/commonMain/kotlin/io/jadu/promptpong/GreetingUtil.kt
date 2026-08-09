9f010d6d820aa6f72d6d397f89ac15dce1bf5810
jadu <rohitkumarccc68@gmail.com>
2026-08-09 18:43:54 +0530

    init

---
+++ shared/src/commonMain/kotlin/io/jadu/promptpong/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package io.jadu.promptpong
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/io/jadu/promptpong/MainViewController.kt
@@ -0,0 +1,5 @@
+package io.jadu.promptpong
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
